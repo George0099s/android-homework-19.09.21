69c8509ccc24a03f5e9a88ff3c96dbe894d3aeb5
aakudr <aakudr2k3@gmail.com>
2021-09-19 19:02:03 +0300

    Added image loading functionality

--- app/src/main/java/com/example/vkvezdekod/Fragment3.kt
+++ app/src/main/java/com/example/vkvezdekod/Fragment3.kt
@@ -1,55 +1,47 @@
 package com.example.vkvezdekod
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import kotlinx.android.synthetic.main.fragment_2.*
+import kotlinx.android.synthetic.main.fragment_2.button_target
+import kotlinx.android.synthetic.main.fragment_3.*
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Fragment3.newInstance] factory method to
- * create an instance of this fragment.
- */
-class Fragment3 : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class Fragment3: Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    companion object {
+        val IMAGE_REQUEST_CODE = 100
     }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_3, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Fragment3.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic fun newInstance(param1: String, param2: String) =
-                Fragment3().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        cover_picker.setOnClickListener {
+            loadCover()
+        }
+    }
+
+    private fun loadCover() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK) {
+            cover_picker.setImageURI(data?.data)
+        }
     }
 }
--- app/src/main/java/com/example/vkvezdekod/MainActivity.kt
+++ app/src/main/java/com/example/vkvezdekod/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.vkvezdekod
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.fragment_3.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            Fragment3.IMAGE_REQUEST_CODE -> cover_picker.setImageURI(data?.data)
+        }
     }
 }
